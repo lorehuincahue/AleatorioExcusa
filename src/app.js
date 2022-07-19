c24c04461fb4d1eb02c7c3c242c1dda91d43805c
lorehuincahue <lorehuincahue@gmail.com>
2022-07-19 01:40:07 +0000

    listo :)

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My granma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let ex1 = Math.floor(Math.random() * who.length);
+  let ex2 = Math.floor(Math.random() * action.length);
+  let ex3 = Math.floor(Math.random() * what.length);
+  let ex4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[ex1] + " " + action[ex2] + " " + what[ex3] + " " + when[ex4];
 };
